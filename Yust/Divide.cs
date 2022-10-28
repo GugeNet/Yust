96c2a312aadd54160bcd56f40b7b4c4c9a40c0b4
Geir E. R. Gundersen <geir.gundersen@fremtind.no>
2022-10-28 11:47:07 +0200

    Implemented division

---
+++ Yust/Divide.cs
@@ -0,0 +1,21 @@
+﻿using EndIf.Yust;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yust
+{
+    [Description(token: "/", precedence: 5)]
+    internal class Divide : Instruction
+    {
+        public override bool Execute(Stack<object> stack, IValueFromKey<string, object> context = null)
+        {
+            var f1 = (int)stack.Pop();
+            var f2 = (int)stack.Pop();
+            stack.Push(f2 / f1);
+            return true;
+        }
+    }
+}
